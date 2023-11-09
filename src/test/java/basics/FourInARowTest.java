fa5aa18263ae5ab5aad8fc40ef6a9456f302f5be
Pierre Schaus <pschaus@gmail.com>
2023-11-09 16:03:24 +0100

    added tests, thank you @vfairon 🙏

--- src/test/java/basics/FourInARowTest.java
+++ src/test/java/basics/FourInARowTest.java
@@ -51,4 +51,93 @@ public class FourInARowTest {
 
     }
 
+    @Test
+    @Grade(value = 1, cpuTimeout = 1000)
+    public void test3() { //up right  ↗ diagonal
+
+        FourInARow game = new FourInARow();
+        game.play(0, 'X');
+
+        game.play(1, 'O');
+        game.play(1, 'X');
+
+        game.play(2, 'O');
+        game.play(2, 'O');
+        game.play(2, 'X');
+
+        game.play(3, 'O');
+        game.play(3, 'O');
+        game.play(3, 'O');
+        game.play(3, 'X');
+
+        assertTrue(game.hasWon('X'));
+        assertFalse(game.hasWon('O')); // if O won, then condition on 3 and not 4
+
+
+    }
+
+    @Test
+    @Grade(value = 1, cpuTimeout = 1000)
+    public void test4() { //down right  ↘ diagonal
+
+        FourInARow game = new FourInARow();
+        game.play(4, 'X');
+
+        game.play(3, 'O');
+        game.play(3, 'X');
+
+        game.play(2, 'O');
+        game.play(2, 'O');
+        game.play(2, 'X');
+
+        game.play(1, 'O');
+        game.play(1, 'O');
+        game.play(1, 'O');
+        game.play(1, 'X');
+
+        assertTrue(game.hasWon('X'));
+        assertFalse(game.hasWon('O')); // if O won, then condition on 3 and not 4
+
+
+    }
+
+    @Test
+    @Grade(value = 1, cpuTimeout = 1000)
+    public void test5() {
+
+        FourInARow game = new FourInARow();
+        game.play(0, 'X');
+        game.play(1, 'X');
+        game.play(2, 'X');
+
+        assertFalse(game.hasWon('X')); // if X won, then condition on 3 and not 4
+        assertFalse(game.hasWon('O'));
+
+        game.play(3, 'X');
+
+        assertTrue(game.hasWon('X')); // if X won, then condition on 3 and not 4
+        assertFalse(game.hasWon('O')); // if O won there but not previous Assert then good luck
+
+
+    }
+
+
+    @Test
+    @Grade(value = 1, cpuTimeout = 1000)
+    public void test6() { //down right  ↘ diagonal
+
+        FourInARow game = new FourInARow();
+        game.play(6, 'X');
+        game.play(5, 'X');
+        game.play(4, 'X');
+        game.play(3, 'X');
+
+
+
+        assertTrue(game.hasWon('X'));
+        assertFalse(game.hasWon('O')); // if O won, then condition on 3 and not 4
+
+
+    }
+
 }
